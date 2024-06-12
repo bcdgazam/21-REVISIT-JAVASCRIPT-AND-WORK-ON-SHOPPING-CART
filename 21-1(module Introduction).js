fbf8371612ba0ba1933182e359db45ee18610f20
Golam Azam <bcdazam@gmail.com>
2024-06-13 02:27:09 +0600

    Curriculum

---
+++ 21-1(module Introduction).js
@@ -0,0 +1,7 @@
+//21-1 Module Introduction Revisit JavaScript
+
+/* 
+
+    We discussed What will be discussed in this module.
+
+*/
---
+++ 21-2(Curriculum).js
@@ -0,0 +1,58 @@
+//21-2 How to learn any Programming Language
+
+/*
+
+    learning path like: s curve ;
+
+    Must have to learn:
+        1. Variable
+        2. conditional (If, else)
+        3. Array 
+        4. For Loop
+        5. Function (most Important)
+        6. Object
+
+        less important
+        7. event bubble
+        8. Dom
+*/
+
+/*
+Curriculum Batch 2: working file
+========================================
+Revisit JavaScript
+    1. What important to beginners and what not
+    2. Variable- name when to declare, etc.
+Things you will need absolute to go to Javascript
+    1. How to learn JavaScript
+    2. Variable
+        a. Variable type (boolean, number, string)
+        b. condition,
+        c. array,
+        d. loop (for, while).
+        e. function, object
+        f. namingConvention
+        g. object
+3. Proble solving-- Just see how things will work
+4. Good to know: Vanila JS or interview or just understand core part
+        a. Switch
+        b. DOM, nodelist, nodetype
+        c. getElementByld, getElementsByClassName, querySelector, querySelectorAll
+        d. appendChild
+        e. addEventListener
+5. Will be advanced [for interview or better developer]
+        a. Call back function
+        b. Event bubble
+6. Future Modules
+        a. Map, filter. Find
+        b. Not necessary
+            i. Bind call apply
+Event, types of event (mouse, keyboard, page loading, browser)
+4 ways to add Eventhandler to an element
+    Meaningful name
+    Return type
+    Negative value check
+    Code formatting
+    Excessive new line
+
+*/
