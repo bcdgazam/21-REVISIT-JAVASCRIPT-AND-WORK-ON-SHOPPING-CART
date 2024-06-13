37c37636c4698e9fd6b2d6972b584db539e18e5f
Golam Azam <bcdazam@gmail.com>
2024-06-13 23:04:47 +0600

    brick calculator

---
+++ 21-3 name variable and where to declare variable.js
@@ -0,0 +1,48 @@
+//21-3 How to name variable and where to declare variable
+
+//* Convert Feet item to mile 
+/*
+function footToMile(foot){
+    if(foot < 0){
+        console.log('You may not entered valid input. Please check!');
+    }
+    let miles = parseFloat(0.000189394 * foot);
+    console.log('Value in Mile : ' + miles.toFixed(6));
+}
+
+//! Output Check
+footToMile(10)
+
+// Wood Calculator
+function woodCalculator(chair, table, bed){
+    if(chair < 0 || table < 0 || bed < 0){
+        console.log('Please Enter valid input');
+    }
+    totalWood = (chair * 1 ) + (table * 3) + (bed * 5);
+    console.log('Total wood needed : ' + totalWood + ' ft.');
+}
+
+woodCalculator(1, 5, 10);
+
+*/
+
+// Brick Calculator
+function brickCalculator(floor){
+    let totalBrick;
+    if (floor <= 0) {
+        console.log('Sorry! Negative input not allowed.');
+        return;
+    }
+    if (floor <= 10) {
+        totalBrick = floor * 15 * 1000;
+    }
+    else if (floor <= 20) {
+        totalBrick = (10 * 15 * 1000) + ((floor - 10) * 12 * 1000);
+    }
+    else{
+        totalBrick = (10 * 15 * 100) + (10 * 12 * 100) + ((floor -  20) * 10 * 1000);
+    }
+    console.log('Total brick need to build the building: ' + totalBrick);
+}
+
+brickCalculator(5);
